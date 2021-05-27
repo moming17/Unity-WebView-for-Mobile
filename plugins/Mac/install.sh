e778e856f38cae9b426c3fd79e973c607729002d
Sercan Altundas <sercan.altundas@gmail.com>
2021-05-27 18:55:24 +0300

    updated from RPM

--- plugins/Mac/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-DSTDIR="../../build/Packager/Assets/Plugins"
-rm -rf DerivedData
-xcodebuild -target WebView -configuration Release -arch i386 -arch x86_64 build CONFIGURATION_BUILD_DIR='DerivedData' | xcpretty
-mkdir -p $DSTDIR
-
-cp -r DerivedData/WebView.bundle $DSTDIR
-rm -rf DerivedData
-cp *.bundle.meta $DSTDIR
